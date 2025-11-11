6140104d0b9e50cd29bb8f67f54105cc6b26fb61
Raúl <al202400224itid@gmail.com>
2025-11-11 09:21:23 -0700

    SI

--- servicios.php
+++ servicios.php
@@ -12,9 +12,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Optica - Servicios</title>
     <link rel="stylesheet" href="./Components/Header/style.css">
+    <link rel="stylesheet" href="./CSS/productos.css">
 </head>
 <body>
     <?php include './Components/Header/header_servicios_home.php'; ?>
     
+    <main>
+    <div class="product-card">
+        <p style="color:var(--green);font-weight:600"><a href="./App/Views/form_reservar_cita.php">Nueva cita</a></p>
+    </div>
+    </main>
+    
+
 </body>
 </html>
